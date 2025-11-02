cee4d81fb26d7c801661e97054fdf4440170d47b
kikerevelo <mreveloescobar@gmail.com>
2025-11-02 15:04:20 +0100

    domingo

--- Javascript/14.js
+++ Javascript/14.js
@@ -13,13 +13,37 @@ console.table(arregloMixto);
 console.log(arregloMixto[400]);// te da undefined si no existe el indice
 
 //lenth
-console.log(arregloMixto.length);
-console.log(arregloMixto[4])
- console.log(arregloMixto[0])
- console.log(arregloMixto[1])
-  console.log(arregloMixto[5])
+// console.log(arregloMixto.length);
+// console.log(arregloMixto[4])
+//  console.log(arregloMixto[0])
+//  console.log(arregloMixto[1])
+//   console.log(arregloMixto[5])
 
-//Recorrer un arreglo
-numeros.forEach(function(numero,index){
-    console.log(`${index} : ${numero}`);
-});
+// Recorrer un arreglo
+// numeros.forEach(function(numero,index){
+//     console.log(`${index} : ${numero}`);
+// });
+//lOS METODOS DE LOS ARREGLOS
+
+numeros[5]=60;//agregar un elemento al final del arreglo
+// usamos push
+numeros.push(70);
+console.log(numeros)
+console.table(numeros);
+//unshift agrega elementos al inicio del arreglo
+numeros.unshift(25);
+console.table(numeros);
+
+//Eliminar elementos
+//pop elimina el ultimo elemento del arreglo
+numeros.pop(5);
+console.table(numeros);
+
+//eñl uso de shift es para eliminar el primer elemento del arreglo
+numeros.shift();
+console.table(numeros);
+
+
+console.log(meses);
+meses.splice(2,1);//elimina a partir del indice 2 un elemento
+console.table(meses);
---
+++ Javascript/15.js
@@ -0,0 +1,71 @@
+const meses=['Enero','Febrero','Marzo','Abril','Mayo'];
+console.table(meses);   
+
+//CARRITO DE COMPRAS
+
+const carrito=[
+    {nombre:'Monitor 20 Pulgadas',precio:500},
+    {nombre:'Television 50 Pulgadas',precio:700},       
+    {nombre:'Tablet',precio:300},
+    {nombre:'Audifonos',precio:200},
+    {nombre:'Teclado',precio:50},
+    {nombre:'Celular',precio:500},
+];  
+console.table(carrito);
+
+// queremos ver s existe marzo en el arreglo meses
+console.log(meses)
+
+meses.forEach(function(mes){
+if(mes=='Marzo'){
+    console.log('Marzo si existe');
+}else{
+    console.log('No existe');
+    }
+}); 
+
+// usando includes
+if(meses.includes('Diciembre')){
+       console.log('Si, EXISTE')
+}else{
+    console.log('No, NO EXISTE')
+    }
+;
+//usando some en meses, sirve para saber si al menos un elmento existe llamandose asi
+const existeEnero=meses.some(function(mes){
+    return mes==='Enero';;
+});
+console.log('Usando some en meses:,     ${existeEnero}')
+
+//usando some en carrito de compras con funcion arrow
+const existeTablet=carrito.some(producto=>producto.nombre==='Tablet');
+console.log('Usando some en carrito de compras ${existeTablet}');
+
+let resultado;
+resultado=carrito.some(function(producto){
+    return producto.nombre==='Celular';
+});
+console.log(resultado);
+//metodo reduce para sumar los precios del carrito de compras
+resultado=carrito.reduce(function(total,producto){
+    return total+producto.precio;
+
+},0); // el cero es el valor inicial de total
+
+console.log (`La suma   de los  articulos del carrito es:${resultado}  dolares`);
+
+//ahora usando arrow fuction
+resultado=carrito.reduce((total,producto)=>total+producto.precio,0);
+console.log (`Usando arrow function La suma   de los  articulos del carrito es:${resultado}  dolares`);
+
+
+//metodo filter para filtrar elementos de un arreglo
+resultado=carrito.filter(function(producto){
+    return producto.precio>400;
+})
+console.table(resultado);
+
+//usando arrow function
+resultado=carrito.filter(producto=>producto.precio<400);
+
+console.table(resultado);
